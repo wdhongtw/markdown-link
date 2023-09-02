3f3d1b01bcb664b4e8b8befb726f20e99d88a2cd
JayBeeDe <jean.baptiste.delon@outlook.com>
2023-09-03 01:39:53 +0200

    add configurable regex options

--- background.js
+++ background.js
@@ -1,29 +1,44 @@
-
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    var link = '[' + tab.title + '](' + tab.url + ')';
+chrome.browserAction.onClicked.addListener(function (tab) {
 
-    element = document.createElement("textarea");
-    element.value = link;
+    title = tab.title;
+    chrome.storage.sync.get(null, (storage) => {
+        for (index in storage.fieldsets) {
+            regexUrl = new RegExp(storage.fieldsets[index].url, 'gi');
+            if (regexUrl.test(tab.url)) {
+                regexSearch = new RegExp(storage.fieldsets[index].search, 'gi');
+                regexReplace = new RegExp(storage.fieldsets[index].replace, 'i');
+                if (regexSearch.test(title)) {
+                    title = title.replace(regexSearch, storage.fieldsets[index].replace);
+                    // we don't stop in case there are several rules
+                }
+            }
+        }
 
-    // It's required to put the temporary element to document tree before copy command
-    // See: https://stackoverflow.com/questions/25622359/clipboard-copy-paste-on-content-script-chrome-extension
-    document.body.appendChild(element);
-    element.select();
-    document.execCommand('copy');
+        var link = '[' + title + '](' + tab.url + ')';
 
-    element.remove();
+        element = document.createElement('textarea');
+        element.value = link;
 
-    chrome.browserAction.setBadgeText({
-        text: "Done",
-        tabId: tab.id
-    });
+        // It's required to put the temporary element to document tree before copy command
+        // See: https://stackoverflow.com/questions/25622359/clipboard-copy-paste-on-content-script-chrome-extension
+        document.body.appendChild(element);
+        element.select();
+        document.execCommand('copy');
 
-    var clear_callback = function () {
+        element.remove();
+
         chrome.browserAction.setBadgeText({
-            text: "",
+            text: 'Done',
             tabId: tab.id
         });
-    };
-    window.setTimeout(clear_callback, 1500);
+
+        var clear_callback = function () {
+            chrome.browserAction.setBadgeText({
+                text: '',
+                tabId: tab.id
+            });
+        };
+        window.setTimeout(clear_callback, 1500);
+    });
 });
